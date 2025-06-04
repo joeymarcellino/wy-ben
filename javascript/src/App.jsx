29ea61b88059f09e6a2cd5fa04ec828015a2c827
Joey Marcellino <joey.marcellino@gmail.com>
2025-06-04 15:05:55 -0700

    added household income page

--- javascript/src/App.jsx
+++ javascript/src/App.jsx
@@ -54,13 +54,38 @@ function HouseholdEarnedIncome({onNext,onBack,dataForm,updateDataForm}) {
   	if ((/^$|^(\d+\,?)+(\.?\d{0,2})$/).test(value)) {
 		updateDataForm((prevData) => ({
 			...prevData, hhEarnedIncome: value}))	
-       	  }
-	}
+     	}
+  }
 
   const handleBlur = (e) => {
-	const value = e.target.value
-	const [whole,decimal] = value.split(".")
+	const raw = e.target.value
+	if (!raw) return
+	
+	const strArray = raw.split(".")
+	const whole = strArray[0].replace(/\,/g,"")
+	const whole_formatted = Number(whole).toLocaleString()
+
+	let decimal_formatted = ""
+	if (strArray.length === 1) {
+		decimal_formatted = ".00"
+	}	
+	else {
+		const decimal = strArray[1]
+		if (decimal.length === 0) {
+			decimal_formatted = ".00"
+		}
+		else if (decimal.length === 1) {
+			decimal_formatted = "." + decimal + "0"
+		}
+		else {
+			decimal_formatted = "." + decimal
+		}
 	}
+	
+	const value_formatted = whole_formatted + decimal_formatted
+	updateDataForm((prevData) => ({
+		...prevData, hhEarnedIncome: value_formatted}))
+  }
 
   return (
 	<>
